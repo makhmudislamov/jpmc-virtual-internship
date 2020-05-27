ce416c7a772dc661d7c78ae8dde4923e4c37cae8
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-05-27 03:39:59 -0700

    streaming the data continuously

--- JPMC-tech-task-2-PY3/src/App.tsx
+++ JPMC-tech-task-2-PY3/src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -21,26 +22,43 @@ class App extends Component<{}, IState> {
     this.state = {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
+      // the graph will not show until user clicks "Start Streaming Data"
       data: [],
+      showGraph: false,
     };
   }
 
   /**
    * Render Graph react component with state.data parse as property data
+   * rendering the graph if showGraph is true
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let x = 0;
+    // Update the state by creating a new array of data that consists of
+    // Previous data in the state and the new data from server
+    // this.setState({ data: [...this.state.data, ...serverResponds] });
+    // getting data continuously. User doesn't have to click the button
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState ({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
