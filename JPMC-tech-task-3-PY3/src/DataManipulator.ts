2229521a0e3ae2a10acf8321bf48f3dad7af4bee
Makhmud Islamov <makhmud.islamov@students.makeschool.com>
2020-05-27 17:15:15 -0700

    uploaded certificate

--- JPMC-tech-task-3-PY3/src/DataManipulator.ts
+++ JPMC-tech-task-3-PY3/src/DataManipulator.ts
@@ -16,8 +16,8 @@ export class DataManipulator {
     const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
     const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
-    const upperBound = 1 + 0.07;
-    const lowerBound = 1 - 0.07;
+    const upperBound = 1 + 0.03;
+    const lowerBound = 1 - 0.03;
     return {
       price_abc: priceABC,
       price_def: priceDEF,
